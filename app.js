7f25b444b7becf868713064853ab57460d0dc60f
arvin <arvin.caparros.code@gmail.com>
2022-01-04 09:52:00 +0800

    5th Commit

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
-// const form = document.querySelector('form');
-// const userName = document.querySelector('#user-name');
-// const passWord = document.querySelector('#password');
-// const showAlert = document.querySelector('.show-alert');
+const switchButton = document.querySelector('.switch');
 
-// form.addEventListener('submit', function(e) {
-
-//   if (userName.value !== "" && passWord.value !== "") {
-
-//     // Loading
-//     document.querySelector('.loading').style.display = 'flex';
-//     // Disappear after 3.5 seconds
-//     setTimeout(() => {
-//       document.querySelector('.loading').style.display = 'none';
-//     }, 3500);
-    
-    
-//   } else {
-//     // Show alert
-//     showAlert.style.display = 'block';
-//     showAlert.innerHTML = `<p>Please fill in all fields.</p>`;
-//     // Disappear alert after 3 seconds
-//     setTimeout(() => {
-//       showAlert.style.display = 'none';
-//     }, 3000);
-    
-//   }
-
-//   // Clear Fields
-//   clearField();
-  
-  
-  
- 
-//   e.preventDefault();
-// })
-
-// // clear fields
-// function clearField() {
-//   userName.value = '';
-//   passWord.value = '';
-// }
+switchButton.addEventListener('click', function() {
+  document.querySelector('.icon').classList.toggle('toggleSwitch');
+  // document.querySelector('.icon').innerHTML = '<i class="fas fa-sun"></i>';
+  document.body.classList.toggle('toggleBody');
+  document.querySelector('.form').classList.toggle('toggleForm');
+})
